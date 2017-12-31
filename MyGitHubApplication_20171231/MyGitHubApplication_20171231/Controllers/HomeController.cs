eab87f5b535a102e6f52e06ca2a594b97133ea2a
Daniel Paduck <daniel.paduck@perkinswill.com>
2017-12-31 08:33:02 -0600

    My First Commit All And Push

---
+++ MyGitHubApplication_20171231/MyGitHubApplication_20171231/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyGitHubApplication_20171231.Models;
+
+namespace MyGitHubApplication_20171231.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+			//hello View();
+			return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MyGitHubApplication_20171231/MyGitHubApplication_20171231/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyGitHubApplication_20171231.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
